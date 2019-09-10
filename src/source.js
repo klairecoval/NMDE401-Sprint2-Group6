82493b1e16fc79a8e3968ef23aab5ce61f8ff284
Edenhype <edenhype@gmail.com>
2019-09-09 20:19:15 -0400

    fixed restarting + card hover

--- src/source.js
+++ src/source.js
@@ -1,4 +1,5 @@
 var correctCards = 0;
+var dbgWin = 0
 $( init );
 var timer2 = "01:00";
 
@@ -40,81 +41,15 @@ function init() {
     cardPile.appendChild(cardPile.children[Math.random() * i | 0]);
   }
 
-  // Play audio on Cards
-  $("#card1").mouseover(function() {
-    $("#audio1")[0].play();
-  })
-  $("#card2").mouseover(function() {
-    $("#audio2")[0].play();
-  })
-  $("#card3").mouseover(function() {
-    $("#audio3")[0].play();
-  })
-  $("#card4").mouseover(function() {
-    $("#audio4")[0].play();
-  })
-  $("#card5").mouseover(function() {
-    $("#audio5")[0].play();
-  })
-  $("#card6").mouseover(function() {
-    $("#audio6")[0].play();
-  })
-  $("#card7").mouseover(function() {
-    $("#audio7")[0].play();
-  })
-  $("#card8").mouseover(function() {
-    $("#audio8")[0].play();
-  })
-  $("#card9").mouseover(function() {
-    $("#audio9")[0].play();
-  })
-  $("#card10").mouseover(function() {
-    $("#audio10")[0].play();
-  })
-  $("#card11").mouseover(function() {
-    $("#audio11")[0].play();
-  })
-  $("#card12").mouseover(function() {
-    $("#audio12")[0].play();
-  })
-
-  // Pause audio on Cards
-  $("#card1").mouseout(function() {
-    $("#audio1")[0].pause();
-  })
-  $("#card2").mouseout(function() {
-    $("#audio2")[0].pause();
-  })
-  $("#card3").mouseout(function() {
-    $("#audio3")[0].pause();
-  })
-  $("#card4").mouseout(function() {
-    $("#audio4")[0].pause();
-  })
-  $("#card5").mouseout(function() {
-    $("#audio5")[0].pause();
-  })
-  $("#card6").mouseout(function() {
-    $("#audio6")[0].pause();
-  })
-  $("#card7").mouseout(function() {
-    $("#audio7")[0].pause();
-  })
-  $("#card8").mouseout(function() {
-    $("#audio8")[0].pause();
-  })
-  $("#card9").mouseout(function() {
-    $("#audio9")[0].pause();
-  })
-  $("#card10").mouseout(function() {
-    $("#audio10")[0].pause();
-  })
-  $("#card11").mouseout(function() {
-    $("#audio11")[0].pause();
-  })
-  $("#card12").mouseout(function() {
-    $("#audio12")[0].pause();
-  })
+  for(let i = 1; i < 13; i++) {
+    $(`#card${i}`).mouseover(function() {
+      $(`#audio${i}`)[0].play();
+    })
+    $(`#card${i}`).mouseout(function() {
+      $(`#audio${i}`)[0].pause();
+      $(`#audio${i}`)[0].currentTime = 0;
+    })
+  }
 
   // Create the card slots
   for ( var i=1; i<=12; i++ ) {
@@ -144,8 +79,9 @@ function handleCardDrop( event, ui ) {
   
   // If all the cards have been placed correctly then display a message
   // and reset the cards for another go
-  if ( correctCards === 12 ) {
+  if ( dbgWin || correctCards === 12 ) {
     $('#successMessage').show();
+    clearInterval(interval)
     $("#audioFull")[0].play();
   }
 }
